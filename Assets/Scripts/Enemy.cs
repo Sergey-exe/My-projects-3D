bd447a3bb58ff9794574715661bfe94d1ab1012c
Draft <140250607+Sergey-exe@users.noreply.github.com>
2024-03-31 14:50:01 +0300

    Generation of enemies per level

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,22 +7,21 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private Transform[] _waypoints;
 
-    public event Action CurrentWaypoint;
-
-    private int _currentWaypoint = 0;
-    public bool LastPoint => _currentWaypoint + 1 == _waypoints.Length - 1;
-
-    private void Start()
-    {
-
-    }
+    [SerializeField] private int _currentWaypoint = 0;
 
     private void Update()
     {
         if (transform.position == _waypoints[_currentWaypoint].position)
         {
-            _currentWaypoint = (_currentWaypoint + 1) % _waypoints.Length;
-            CurrentWaypoint?.Invoke();
+            _currentWaypoint = (_currentWaypoint + 1);
+
+            if (_currentWaypoint == _waypoints.Length)
+                Destroy(gameObject);
+        }
+
+        if(_currentWaypoint >= _waypoints.Length)
+        {
+            return;
         }
 
         transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentWaypoint].position, _speed * Time.deltaTime);
