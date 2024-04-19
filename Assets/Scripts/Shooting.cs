2b371d9a64413fc68222a8a7bd38d8a6fc566588
Draft <140250607+Sergey-exe@users.noreply.github.com>
2024-04-19 21:18:26 +0300

    FFF

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private float _speedBullet;
+    [SerializeField] private float _delayShot;
+
+    [SerializeField] private Rigidbody _bullet;
+    [SerializeField] private Transform _target;
+
+    private void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    private IEnumerator Shoot()
+    {
+        bool isWork = true;
+
+        var wait = new WaitForSeconds(_delayShot);
+
+        while (isWork)
+        {
+            Vector3 direction = (_target.position - transform.position).normalized;
+
+            Rigidbody newBullet = Instantiate(_bullet, transform.position + direction, Quaternion.identity);
+
+            newBullet.transform.up = direction;
+            newBullet.velocity = direction * _speedBullet;
+
+            yield return wait;
+        }
+    }
+}
